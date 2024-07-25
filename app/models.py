95a89c4b5505531a2a3b50cd511d9516355cb909
Miguel Alejandro Cepeda <37114980+alejomjc@users.noreply.github.com>
2024-07-24 19:49:27 -0500

    Feat: Add docstring in models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,28 @@
+"""
+This module defines the schema for login requests using Pydantic.
+"""
+
 from pydantic import BaseModel
 
-
 class LoginSchema(BaseModel):
+    """
+    Schema for validating login requests.
+
+    Attributes:
+        email (str): The email address of the user.
+        password (str): The password for the user account.
+    """
+
     email: str
     password: str
 
     class Config:
+        """
+        Configuration for the LoginSchema.
+
+        Attributes:
+            schema_extra (dict): Example data for the schema.
+        """
         schema_extra = {
             "example": {
                 "email": "test@mail.com",
