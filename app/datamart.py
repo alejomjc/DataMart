383ab0bb6ff837f02c4b9934342729747b1bb886
Miguel Alejandro Cepeda <37114980+alejomjc@users.noreply.github.com>
2024-07-24 19:48:30 -0500

    Feat: Add main docstring and fixes from pylinter in datamart.py

--- app/datamart.py
+++ app/datamart.py
@@ -1,8 +1,19 @@
-import pandas as pd
-import glob
+"""
+This module loads and concatenates parquet files into a single pandas DataFrame.
 
-file_path = 'app/data/'
-all_files = glob.glob(file_path + "data_chunk*.snappy.parquet")
+It performs the following operations:
+- Defines the file path for parquet files.
+- Retrieves all file paths matching the pattern for parquet files.
+- Reads each parquet file into a pandas DataFrame.
+- Concatenates all individual DataFrames into a single DataFrame.
+"""
+
+import glob
+import pandas as pd
+
+
+FILE_PATH = 'app/data/'
+all_files = glob.glob(FILE_PATH + "data_chunk*.snappy.parquet")
 
 df_list = [pd.read_parquet(file) for file in all_files]
 df = pd.concat(df_list, ignore_index=True)
